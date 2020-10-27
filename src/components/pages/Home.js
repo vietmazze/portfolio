4231f43683301fca435d5f7c38dbc2e14f953622
vietmazze <dqnguyen2011@gmail.com>
2020-10-26 23:29:36 -0400

    planner

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import PlannerBody from "../plannerComponents/PlannerBody";
 import PnL from "../PnL";
 function Home() {
   return (
     <div>
       <>
-        <PnL />
+        <PlannerBody />
       </>
     </div>
   );
