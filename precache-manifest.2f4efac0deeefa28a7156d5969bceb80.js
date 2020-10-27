79d517cb0261e04bd460f5efef7ec8fb0b99f940
vietmazze <dqnguyen2011@gmail.com>
2020-10-26 23:20:59 -0400

    Updates

---
+++ precache-manifest.2f4efac0deeefa28a7156d5969bceb80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bed64236a439fae37a16fa854124827",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dd9b5d96c3346a881b7c",
+    "url": "/portfolio/static/css/main.d5ccd620.chunk.css"
+  },
+  {
+    "revision": "0503185d8e9a815b2a14",
+    "url": "/portfolio/static/js/2.2ed3ec7a.chunk.js"
+  },
+  {
+    "revision": "3b56d375840d1b33181201a78daba96c",
+    "url": "/portfolio/static/js/2.2ed3ec7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9b5d96c3346a881b7c",
+    "url": "/portfolio/static/js/main.b979cc5d.chunk.js"
+  },
+  {
+    "revision": "c5faab4ad124c1f62963",
+    "url": "/portfolio/static/js/runtime-main.26b263a1.js"
+  },
+  {
+    "revision": "646eaa9be30fa40f2782b4a50909691f",
+    "url": "/portfolio/static/media/phone-yosemite.646eaa9b.jpg"
+  },
+  {
+    "revision": "63bbdb5b76b8d349ad35ff4281efbd37",
+    "url": "/portfolio/static/media/yosemite-8.63bbdb5b.png"
+  }
+]);
