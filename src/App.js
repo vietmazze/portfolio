7ba275c986e4d7ba3f13954f5d124497a30dd48b
vietmazze <dqnguyen2011@gmail.com>
2020-11-15 10:42:47 -0500

    added authentication

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/pages/Home";
 import Navbar from "./components/Navbar";
 import PnL from "./components/PnL";
 import Planner from "./components/pages/Planner";
+import Signup from "./components/pages/Signup";
 import { TimeProvider } from "./components/plannerComponents/pomodoro/TimerProvider";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -23,6 +24,7 @@ function App() {
               </TimeProvider>
             )}
           />
+          <Route path="/sign-up" exact component={Signup} />
         </Switch>
       </Router>
     </div>
